a43b060cad880d8505317f6de269685933914f05
mayur7264 <mayuryadav7264@gmail.com>
2021-01-07 10:54:04 +0530

    final

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <Navbar/>
       
         <Switch>
-          <Route exact path='/' component={Login}/>
+          <Route exact path='/advisorPortal' component={Login}/>
           <Route exact path='/login' component={Login}/>
           <Route exact path='/register' component={Register}/>
           <Route exact path='/home' component={Home}/>
